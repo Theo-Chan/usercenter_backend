83f3332f45acc36adb005e46344e40bb4bba2ca9
adbam <chenjialuo_theo@163.com>
2023-07-14 14:18:40 +0800

    add userForm

---
+++ src/test/java/com/theo/usercenterbackend/sampleTest.java
@@ -0,0 +1,18 @@
+package com.theo.usercenterbackend;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author Theo
+ * @version 1.0
+ * Create by 2023/7/13 22:54
+ */
+@SpringBootTest
+public class sampleTest {
+
+}
